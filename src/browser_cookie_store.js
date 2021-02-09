62e2c747925adb5393f8c92d19fbb24d7b70044c
3wnbr1 <ewen.brun@icloud.com>
2021-02-09 09:51:46 +0100

    Updated cookie name

--- src/browser_cookie_store.js
+++ src/browser_cookie_store.js
@@ -1,19 +1,16 @@
 import Cookies from 'js-cookie';
 
-const generateKey = (clientId, namespace) => `${namespace}-${clientId}-token`;
-
 const createStore = ({ clientId, secure }) => {
   const expiration = 30; // 30 days
-  const namespace = 'st';
-  const key = generateKey(clientId, namespace);
+  const name = 'mgc-auth-token';
 
-  const getToken = () => Cookies.getJSON(key);
+  const getToken = () => Cookies.getJSON(name);
   const setToken = tokenData => {
     const secureFlag = secure ? { secure: true } : {};
-    Cookies.set(key, tokenData, { expires: expiration, ...secureFlag });
+    Cookies.set(name, tokenData, { expires: expiration, ...secureFlag });
   };
   const removeToken = () => {
-    Cookies.remove(key);
+    Cookies.remove(name);
   };
 
   return {
